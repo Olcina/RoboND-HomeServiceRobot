3d4182ac0f3997281f2c049d5f2e3ac56fdf5814
Olcina <karlos.olcina@gmail.com>
2018-06-03 01:26:14 +0200

    feat: premap plus test_navigation stack

---
+++ catkin_ws/src/homeservice/ShellScripts/test_navigation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SOURCE_CMD="source /home/workspace/catkin_ws/devel/setup.bash"
+
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo turtlebot_world.launch " & sleep 5
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo amcl_demo.launch " & sleep 5
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
