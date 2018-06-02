401b2688cb8310c52310c6a814f7118d518bfc27
Olcina <karlos.olcina@gmail.com>
2018-06-02 23:13:39 +0200

    feat: more maps

--- catkin_ws/src/homeservice/ShellScripts/wall_follower.sh
+++ catkin_ws/src/homeservice/ShellScripts/wall_follower.sh
@@ -5,10 +5,10 @@ SOURCE_CMD="source ~/RoboND-HomeServiceRobot/catkin_ws/devel/setup.bash"
 
 terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo turtlebot_world.launch " & sleep 5
 
-terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo kinect_gmapping.launch " & sleep 5
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo gmapping_wall_follower.launch " & sleep 5
 
 terminator -e "$SOURCE_CMD; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
 
-terminator -e "$SOURCE_CMD; roslaunch turtlebot_teleop keyboard_teleop.launch" & sleep 5
+terminator -e "$SOURCE_CMD; rosrun wall_follower wall_follower_node" & sleep 5
 
 
