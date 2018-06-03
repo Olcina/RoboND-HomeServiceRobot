bc714683b29ab44d52e99c0b4d8989702d4cd251
Olcina <karlos.olcina@gmail.com>
2018-06-04 00:35:15 +0200

    feat: home_service

---
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SOURCE_CMD="source ../../devel/setup.bash"
+
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo turtlebot_world.launch " & sleep 5
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo amcl_demo.launch " & sleep 5
+
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
+
+terminator -e "$SOURCE_CMD; rosrun add_markers add_markers_node " & sleep 5
+terminator -e "$SOURCE_CMD; rosrun pick_objects pick_objects_node "
+
