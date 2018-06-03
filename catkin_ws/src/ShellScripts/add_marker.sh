6117a497256a73a65089fcba4a81a7cbd35e8cec
Olcina <karlos.olcina@gmail.com>
2018-06-04 00:35:56 +0200

    feat: add_marker

--- catkin_ws/src/ShellScripts/add_marker.sh
+++ catkin_ws/src/ShellScripts/add_marker.sh
@@ -5,10 +5,9 @@ SOURCE_CMD="source ../../devel/setup.bash"
 
 terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo turtlebot_world.launch " & sleep 5
 
-terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo gmapping.launch " & sleep 5
+terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo amcl_demo.launch " & sleep 5
 
 terminator -e "$SOURCE_CMD; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
 
-terminator -e "$SOURCE_CMD; rosrun wall_follower wall_follower_node" & sleep 5
-
+terminator -e "$SOURCE_CMD; rosrun add_markers add_markers_node " & sleep 5
 
--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -10,5 +10,6 @@ terminator -e "$SOURCE_CMD; roslaunch turtlebot_gazebo amcl_demo.launch " & slee
 terminator -e "$SOURCE_CMD; roslaunch turtlebot_rviz_launchers view_navigation.launch " & sleep 5
 
 terminator -e "$SOURCE_CMD; rosrun add_markers add_markers_node " & sleep 5
+
 terminator -e "$SOURCE_CMD; rosrun pick_objects pick_objects_node "
 
